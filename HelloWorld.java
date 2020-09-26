e111c6dda158718f399a5fe3fe6b4fb407c345db
jmy <sa802786666sa@gmail.com>
2020-09-26 15:32:18 +0800

    dd

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,10 @@
 public class HelloWorld{
     public static void main(String[]args){
        
-<<<<<<< HEAD
-	  public int a;
-=======
+
+
 	  public float a;
->>>>>>> hotfix
+
 	   
 	   System.out.println("Hello World!");
 	    System.out.println("Hello World2!");
