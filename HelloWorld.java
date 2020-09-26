43ddc78ea7a4450799688e7a1a867b96aff8683d
jmy <sa802786666sa@gmail.com>
2020-09-26 15:25:58 +0800

    bb

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,11 @@
 public class HelloWorld{
     public static void main(String[]args){
-       System.out.println("Hello World!");
+       
+	  public float a;
+	   
+	   System.out.println("Hello World!");
+	    System.out.println("Hello World2!");
+	 
     }
 
 
